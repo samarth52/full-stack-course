87242ba0264a98299d73f275285edb7db4805db3
samarth52 <samarthchandna52@gmail.com>
2022-07-07 18:47:05 +0400

    added display country details functionality for case of multiple matches

--- part2/countries/src/components/DisplayMany.js
+++ part2/countries/src/components/DisplayMany.js
@@ -1,7 +1,27 @@
+import { useState } from 'react'
+import DisplayOne from './DisplayOne'
+
 const DisplayMany = ({ countries }) => {
   console.log(`displaying names of ${countries.length} countries`)
+  const [display, setDisplay] = useState(Array(countries.length).fill(false));
+
+  const handleClick = (index) => {
+    const newDisplay = [...display]
+    newDisplay[index] = !display[index]
+    return () => setDisplay(newDisplay)
+  }
+
+  const trial = (index, country) => display[index] ? <DisplayOne country={country}/> : <></>
+
   return <div>
-    {countries.map(country => <div key={country.name.official}>{country.name.common}</div>)}
+    {countries.map((country, index) => {
+      return (
+        <div key={country.name.official}>
+          {country.name.common} <button onClick={handleClick(index)}>{display[index] ? 'hide' : 'show'}</button>
+          {trial(index, country)}
+        </div>
+      )
+    })}
   </div>
 }
 
