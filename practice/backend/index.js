71aa14111a2a8d0e8b012b03cc95943f2393785f
Samarth Chandna <samarthchandna52@gmail.com>
2022-07-11 02:23:12 +0400

    added heroku

--- practice/backend/index.js
+++ practice/backend/index.js
@@ -1,7 +1,8 @@
-const { response } = require('express')
 const express = require('express')
+const cors = require('cors')
 const app = express()
 
+app.use(cors())
 app.use(express.json())
 
 let notes = [
@@ -71,7 +72,7 @@ app.post('/api/notes', (request, response) => {
   response.json(note)
 })
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
