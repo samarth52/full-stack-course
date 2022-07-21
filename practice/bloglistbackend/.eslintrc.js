c4a8db5a9c2e678fd84de5c5366c5f274316072c
Samarth Chandna <samarthchandna52@gmail.com>
2022-07-21 12:38:52 +0400

    incorporated airbnb eslint and created rest test cases

---
+++ practice/bloglistbackend/.eslintrc.js
@@ -0,0 +1,24 @@
+module.exports = {
+  env: {
+    browser: true,
+    commonjs: true,
+    es2021: true,
+    node: true,
+  },
+  extends: [
+    'airbnb-base',
+  ],
+  parserOptions: {
+    ecmaVersion: 13,
+  },
+  rules: {
+    semi: [
+      'error',
+      'never',
+    ],
+    'linebreak-style': [
+      'error',
+      'windows',
+    ],
+  },
+}
--- practice/bloglistbackend/index.js
+++ practice/bloglistbackend/index.js
@@ -1,28 +1,31 @@
-const http = require('http')
+// const http = require('http')
 const express = require('express')
-const app = express()
 const cors = require('cors')
+require('dotenv').config()
 const mongoose = require('mongoose')
+
+const app = express()
+app.use(cors())
+app.use(express.json())
 
 const blogSchema = new mongoose.Schema({
   title: String,
   author: String,
   url: String,
-  likes: Number
+  likes: Number,
 })
 
 const Blog = mongoose.model('Blog', blogSchema)
 
-const mongoUrl = 'mongodb://localhost/bloglist'
-mongoose.connect(mongoUrl)
-
-app.use(cors())
-app.use(express.json())
+const { MONGODB_URI } = process.env
+mongoose.connect(MONGODB_URI)
+  .then(() => console.log('MongoDB Connected!'))
+  .catch((error) => console.log(error.message))
 
 app.get('/api/blogs', (request, response) => {
   Blog
     .find({})
-    .then(blogs => {
+    .then((blogs) => {
       response.json(blogs)
     })
 })
@@ -32,12 +35,12 @@ app.post('/api/blogs', (request, response) => {
 
   blog
     .save()
-    .then(result => {
-      response.status(201).json(result)
+    .then((result) => {
+      response.status(200).json(result)
     })
 })
 
-const PORT = 3003
+const { PORT } = process.env
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
